e59f4640a37a7c770dfa30bf7a5a6dac9cd51463
Shogo <>
2023-06-16 12:44:53 +0000

    全削除

--- src/services/ChordGenerator.ts
+++
@@ -1,9 +0,0 @@
-import Chord from "../models/Chord";
-
-class ChordGenerator {
-    // generateRandomChord(): Chord {
-    //     return new Chord(1, '7#5');
-    // }
-}
-
-export default ChordGenerator;
